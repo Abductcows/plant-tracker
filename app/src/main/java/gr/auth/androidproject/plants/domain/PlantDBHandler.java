fa1a870647212949178a831c826590dda14718db
George Bouroutzoglou <geompokon@csd.auth.gr>
2021-05-20 17:17:46 +0300

    Fix back birthday null check
    It checks for null value instead of "NULL" string. This is evident after 4be73f29a8d84bd91a3ae7da58d98499e30c9555 because there is a NullPointerException now

--- app/src/main/java/gr/auth/androidproject/plants/domain/PlantDBHandler.java
+++ app/src/main/java/gr/auth/androidproject/plants/domain/PlantDBHandler.java
@@ -221,7 +221,7 @@ public class PlantDBHandler extends SQLiteOpenHelper {
 
                     current.setId(cursor.getLong(COLUMN_ID.index)); // ID
                     current.setName(cursor.getString(COLUMN_ID.index)); // NAME
-                    if (!cursor.getString(COLUMN_BIRTHDAY.index).equals("NULL")) { // BIRTHDAY
+                    if (Objects.nonNull(cursor.getString(COLUMN_BIRTHDAY.index))) { // BIRTHDAY
                         current.setBirthday(
                                 LocalDateTime.parse(cursor.getString(COLUMN_BIRTHDAY.index))
                         );
